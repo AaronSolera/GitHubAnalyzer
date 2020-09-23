2f152c773a0b52041ca62460c03a353e564f80e5
Aaron Solera <aaron.smartinez>
2020-09-23 14:27:52 -0600

    Update
    Fixing get_issues() error and adding get_issues_and_pulls()

--- testing.py
+++ testing.py
@@ -33,16 +33,47 @@ roslyn = g.get_repo(github_repo)
 g = check_rate_limit(g)
 
 # Get pull requests
-issue = roslyn.get_issue(1)
-pull = roslyn.get_pull(5)
+issue_no = 1
+pull_no = 3
+issu = roslyn.get_issue(issue_no)
+pull = roslyn.get_pull(pull_no)
+
+issues_comments = roslyn.get_issues_comments()
+pulls_comments = roslyn.get_pulls_comments()
+pulls_review_comments = roslyn.get_pulls_review_comments()
+
+for issues_comment in issues_comments.__iter__():
+    print("Issues comment:",issues_comment.body)
+
+for pulls_comment in pulls_comments.__iter__():
+    print("Pulls comment:",pulls_comment.body)
+
+for pulls_review_comment in pulls_review_comments.__iter__():
+    print("Pulls review comment:", pulls_review_comment.body)
+
+"""
+pull_commnets = roslyn.get_pulls_review_comments()
+issue_commnets = roslyn.get_issues_comments()
+
+for pull_commnet in pull_commnets.__iter__():
+    print(pull_commnet)
+
+print("-"*30)
+
+for issue_commnet in issue_commnets.__iter__():
+    print(issue_commnet)
+
 
 issue_events = issue.get_events()
 pull_events = pull.get_issue_events() 
 
+print("pull as issue -> ", pull.as_issue())
+
 for issue_event in issue_events.__iter__():
-    print("issue -> ", issue_event.event)
+    print("issue ", issue_no, " event -> ", issue_event.event)
 
 for pull_event in pull_events.__iter__():
-    print("pull -> ", pull_event.event, "commit_id -> ")
+    print("pull -> ", pull_no, " event -> ", pull_event.event)
+"""
 
 input("\nPress any key to close...")
